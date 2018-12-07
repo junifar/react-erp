f174eface2ee920486b7db14f79293a1fe4f4c91
junifar <junifar@gmail.com>
2018-12-07 16:47:26 +0700

    Initial

--- src/component/layout/components/control_sidebar.js
+++ src/component/layout/components/control_sidebar.js
@@ -3,7 +3,15 @@ import React, {Component} from 'react'
 class ControlSidebar extends Component{
     render(){
         return(
-            <aside className="control-sidebar control-sidebar-dark"></aside>
+            <aside className="control-sidebar control-sidebar-dark">
+                <ul className="nav nav-tabs nav-justified control-sidebar-tabs">
+                </ul>
+                <div className="tab-content">
+                    <div className="tab-pane" id="control-sidebar-home-tab">
+
+                    </div>
+                </div>
+            </aside>
         )}
 }
 
